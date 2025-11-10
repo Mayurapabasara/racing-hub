dae660d5d54293c37b0c314285d1d2f13fd3065b
Thareesha <thareesha98>
2025-11-10 23:37:04 +0530

    Added new models with configs

---
+++ 02-Business Logic/BaseLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacingHubCarRental
+{
+    /// <summary>
+    /// Represents the basic needs for a logic class!
+    /// </summary>
+    public abstract class BaseLogic : IDisposable
+    {
+
+        /// <summary>
+        /// Holds the database!
+        /// </summary>
+        protected RacingHubCarRentalEntities DB = new RacingHubCarRentalEntities();
+
+        /// <summary>
+        /// Clears database resources!
+        /// </summary>
+        public void Dispose()
+        {
+            DB.Dispose();
+        }
+
+    }
+}
