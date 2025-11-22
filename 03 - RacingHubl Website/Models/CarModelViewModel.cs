c066b082c65de34341f535bd1732661e8eff7689
Thareesha <thareesha98>
2025-11-22 23:44:34 +0530

    Updated models

--- 03 - RacingHubl Website/Models/CarModelViewModel.cs
+++ 03 - RacingHubl Website/Models/CarModelViewModel.cs
@@ -1,30 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System;
 
 namespace RacingHubCarRental
 {
     /// <summary>
-    /// Represents a view model for a car model management.
+    /// ViewModel used for creating or editing car models.
+    /// Contains UI dropdown data and the CarModel entity.
+    /// Completely rewritten following SRP, immutability, and clarity principles.
     /// </summary>
     public class CarModelViewModel
     {
-
         /// <summary>
-        /// Holds the "Manufacturer" and "ManufacturerModel" DropDownLists.
+        /// Gets dropdown values for Manufacturers and Manufacturer Models.
         /// </summary>
-        public ManufacturerModelDDLs DDLs = new ManufacturerModelDDLs();
+        public ManufacturerModelDDLs ManufacturerModelDropdowns { get; }
 
         /// <summary>
-        /// Holds the "Gear" DropDownList.
+        /// Gets dropdown values for Gear Types.
         /// </summary>
-        public GearDDL GearDDL = new GearDDL();
+        public GearDDL GearDropdown { get; }
 
         /// <summary>
-        /// Holds the car model object.
+        /// Gets or sets the CarModel domain object.
         /// </summary>
         public CarModel CarModel { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CarModelViewModel"/> class.
+        /// Safely initializes all dropdowns and ensures CarModel is never null.
+        /// </summary>
+        public CarModelViewModel()
+            : this(new CarModel(), new ManufacturerModelDDLs(), new GearDDL())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance with provided domain object and dropdowns.
+        /// Supports dependency injection and unit testing.
+        /// </summary>
+        public CarModelViewModel(
+            CarModel carModel,
+            ManufacturerModelDDLs manufacturerModelDDLs,
+            GearDDL gearDDL)
+        {
+            CarModel = carModel ?? throw new ArgumentNullException(nameof(carModel));
+
+            ManufacturerModelDropdowns = manufacturerModelDDLs
+                ?? throw new ArgumentNullException(nameof(manufacturerModelDDLs));
+
+            GearDropdown = gearDDL
+                ?? throw new ArgumentNullException(nameof(gearDDL));
+        }
     }
 }
+
